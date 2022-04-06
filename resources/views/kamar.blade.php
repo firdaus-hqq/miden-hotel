f9e4facc4cabd4854c9550d34c9a0285dc1dc2c0
firdaus-hqq <firdaushaqiqi17@gmail.com>
2022-04-06 07:53:32 +0700

    Mengubah tombol thumb kamar

--- resources/views/kamar.blade.php
+++ resources/views/kamar.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.main')
 
-@section('beranda')
-    <div class="container">
-        <img src="images/deluxeraw.jpg" class="d-block w-100" alt="...">
+@section('kamar')
+    <div class="container mt-5">
+        <img src="images/deluxeraw.jpg" class="d-block w-100 mt-5 pt-5" alt="...">
         <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+            <h1>Deluxe</h1>
         </div>
+        <h1 class="text-center text-brown mt-3">Fasilitas Kamar</h1>
+        <p>Serahkan penginapan dan pertemuan Anda ke Miden Hotel. Letak yang strategis,
+            berada di
+            pusat kota membuat anda lebih mudah untuk menjangkau seluruh penjuru kota. Pelayanan terbaik serta
+            fasilitas
+            akan disuguhkan kepada Anda. Kamar dengan harga yang murah dikelasnya, ruang pertemuan dengan
+            kapasitas
+            besar, sangat memanjakan Anda. Kami menyediakan berbagai tipe kamar, sesuaikan dengan budget dan
+            kebutuhan
+            Anda.</p>
     </div>
 @endsection
--- resources/views/thumbkamar.blade.php
+++ resources/views/thumbkamar.blade.php
@@ -3,7 +3,9 @@
         <h1 class="text-center text-brown">Kamar</h1>
         <div class="card card-brown rounded-20 shadow mt-3">
             <div class="card-body">
+                <div>
                 <img src="images/aboutusimage.png" class="img-fluid ps-3 float-end col-lg-4" alt="">
+                <h2 class="text-brown float-start col-lg-8">Deluxe</h2>
                 <p class="text-brown fs-5 float-start col-lg-8">Serahkan penginapan dan pertemuan Anda ke Miden Hotel. Letak yang strategis,
                     berada di
                     pusat kota membuat anda lebih mudah untuk menjangkau seluruh penjuru kota. Pelayanan terbaik serta
@@ -13,8 +15,9 @@
                     besar, sangat memanjakan Anda. Kami menyediakan berbagai tipe kamar, sesuaikan dengan budget dan
                     kebutuhan
                     Anda.</p>
-                    <a href="" class="btn btn-2">Selengkapnya</a>
-                    <a href="" class="btn bg-brown text-white">RESERVASI SEKARANG</a>
+                </div>
+                    <a href="/kamar" class="btn btn-2">Selengkapnya</a>
+                    <a href="/form" class="btn btn-1">RESERVASI SEKARANG</a>
             </div>
         </div>
     </div>
