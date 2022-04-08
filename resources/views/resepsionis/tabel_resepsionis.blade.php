b0e8897d8ed22d9893dbf623208d75cb2d2649e6
firdaus-hqq <firdaushaqiqi17@gmail.com>
2022-04-08 22:16:12 +0700

    Mengubah tulisan pada tab menu

--- resources/views/resepsionis/tabel_resepsionis.blade.php
+++ resources/views/resepsionis/tabel_resepsionis.blade.php
@@ -3,13 +3,13 @@
 @section('tabel_resepsionis')
 <ul class="nav nav-tabs">
     <li class="nav-item">
-      <a class="nav-link {{ ($title === 'Pending') ? 'active' : '' }}" aria-current="page" href="/pending">Pending</a>
+      <a class="nav-link text-black {{ ($title === 'Pending') ? 'active' : '' }}" aria-current="page" href="/pending">Pending</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link {{ ($title === 'On Going') ? 'active' : '' }}" href="/ongoing">On Going</a>
+      <a class="nav-link text-black {{ ($title === 'On Going') ? 'active' : '' }}" href="/ongoing">On Going</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link {{ ($title === 'History') ? 'active' : '' }}" href="/history">History</a>
+      <a class="nav-link text-black {{ ($title === 'History') ? 'active' : '' }}" href="/history">History</a>
     </li>
   </ul>
   @yield('pending')
